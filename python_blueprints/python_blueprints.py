04d8d6ef254c04b320a14ea4527a84b51c1c652f
Fantin Sibony <fantinsibony@gmail.com>
2026-01-25 12:32:42 +0100

    Update : final python blueprints

--- python_blueprints/python_blueprints.py
+++ python_blueprints/python_blueprints.py
@@ -93,17 +93,42 @@ class QE(Scheme):
         return [S0, v0]
     
     def step(self, state, dt):
+
+        def inv_psi(u, p, beta):
+            if (u<= p):
+                return 0 
+            if (p<u) and (u<=1):
+                return (1/beta)*np.log((1-p)/(1-u))
+
         S, v = state
         exp_sp = np.exp(-self.h.kappa * dt)
-        EX = self.theta + (S- self.h.theta) * exp_sp
-        VARX_1 = (S * (self.h.epsilon**2) * exp_sp)*(1-exp_sp)/self.h.kappa
+        EX = self.h.theta + (v- self.h.theta) * exp_sp
+        VARX_1 = (v * (self.h.epsilon**2) * exp_sp)*(1-exp_sp)/self.h.kappa
         VARX_2 = ((self.h.theta * self.h.epsilon**2)*(1-exp_sp)**2)/(2*self.h.kappa)
         VARX = VARX_1 + VARX_2
 
         psi = VARX/EX**2
+        u = np.random.uniform(0,1)
 
-        if psi >= 1.5:
+        if psi > 1.5: #exp regime
+            p = (psi-1)/(psi+1)
+            beta = (1-p)/EX
+            v_next = inv_psi(u, p, beta)
+            v_int = 0.5*(v+v_next)
+            logSt = np.log(S) + self.h.mu*dt - 0.5*v_int*dt+ np.sqrt(v_int*dt)*np.random.normal()
+
+        else: #quadratic regime
+            dpsi = 2/psi
+            b_2 = dpsi -1 + np.sqrt(dpsi*(dpsi-1))
+            a = EX/(1+b_2)
+            Z = np.random.normal()
+            v_next = a*(Z+np.sqrt(b_2))**2
+            v_int = 0.5*(v+v_next)
+            Z_s = self.h.rho*Z + np.sqrt(1-self.h.rho**2)*np.random.normal()
+            logSt = np.log(S) + self.h.mu*dt - 0.5*v_int*dt+ np.sqrt(v_int*dt)*Z_s
             
+        
+        return np.exp(logSt), v_next
 
 
 #----------------------------------------
