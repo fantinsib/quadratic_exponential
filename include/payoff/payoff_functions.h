52568ed4b7262e39ff753fe9916fbdcd258c9d5a
Fantin Sibony <fantinsibony@gmail.com>
2026-03-07 17:53:22 +0100

    Fix : index bug in Extractor

--- include/payoff/payoff_functions.h
+++ include/payoff/payoff_functions.h
@@ -3,5 +3,9 @@
 
 
 class PayoffFunction {
-    double max();
+    
+    //Returns the maximum between A and B
+    double max(double A, double B);
+
+    
 }
--- src/payoff/extractor.cpp
+++ src/payoff/extractor.cpp
@@ -19,7 +19,7 @@ Extractor::Extractor(std::shared_ptr<SimulationResult> res) :
     double dt = T_/static_cast<double>(n_steps_);
     std::vector<double> timeframe(n_steps_ +1);
     
-    for (size_t t =0; t <= n_steps_ +1; t++ ) {
+    for (size_t t =0; t <= n_steps_ ; t++ ) {
         timeframe[t] = dt * t;
     }
 
@@ -30,13 +30,14 @@ Extractor::Extractor(std::shared_ptr<SimulationResult> res) :
 std::vector<std::vector<double>> Extractor::get(ObservationSchedule& schedule) const {
 
     size_t n_spots = schedule.get_n_obs();
+    if (n_spots == 0) throw std::invalid_argument("Extractor::get : received schedule appears empty.");
     std::vector<double> Ts = schedule.get_Ts();
 
     std::vector<size_t> idx = tf_to_index(Ts);
     std::vector<std::vector<double>> spots(n_spots);
     
     for (size_t i = 0; i < n_spots; i++) {
-        spots[i] = res_->get_spot_at(i);
+        spots[i] = res_->get_spot_at(idx[i]);
         
 
     };
